0e359e86d441e559e0ef425ce4ce9253f6dccdce
adorabled4 <dhx2648466390@163.com>
2022-11-17 10:18:33 +0800

    initial commit

--- user-service/src/main/java/cn/itcast/user/UserApplication.java
+++ user-service/src/main/java/cn/itcast/user/UserApplication.java
@@ -3,11 +3,22 @@ package cn.itcast.user;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @MapperScan("cn.itcast.user.mapper")
 @SpringBootApplication
 public class UserApplication {
     public static void main(String[] args) {
         SpringApplication.run(UserApplication.class, args);
+    }
+
+    /**
+     * 创建RestTemplate Bean
+     * @return
+     */
+    @Bean
+    public RestTemplate restTemplate(){
+        return new RestTemplate();
     }
 }
